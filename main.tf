cd40af00b257bb35872bfc32c2e20b9b2bf728c7
lalita2407 <lalitalallu02@gmail.com>
2024-08-27 12:00:08 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_kinesis_stream" "test_stream" {
+  name                      = var.name
+  shard_count               = var.shard_count
+  retention_period          = var.retention_period
+  enforce_consumer_deletion = var.enforce_consumer_deletion
+
+  shard_level_metrics = var.shard_level_metrics
+
+  stream_mode_details {
+    stream_mode = var.stream_mode
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "name" {
+  description = "value"
+  type        = string
+}
+variable "shard_count" {
+  description = "value"
+  type        = number
+}
+variable "retention_period" {
+  description = "value"
+  type        = number
+  default     = 24
+}
+
+variable "enforce_consumer_deletion" {
+  description = "value"
+  type        = bool
+  default     = false
+}
+variable "shard_level_metrics" {
+  description = "value"
+  type        = list(string)
+}
+variable "stream_mode" {
+  description = "value"
+  type        = string
+}
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+
+}
+variable "role_arn" {
+  description = "Role ARN"
+  type        = string
+}
